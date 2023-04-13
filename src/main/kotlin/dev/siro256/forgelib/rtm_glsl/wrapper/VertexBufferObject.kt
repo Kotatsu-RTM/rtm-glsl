54c8625f27231cb3e7ffa5cc62e07ce9a11c5dad
Siro_256 <siro@siro256.dev>
2023-04-13 22:36:20 +0900

    Create multiple types of constructors to VertexBufferObject

--- src/main/kotlin/dev/siro256/forgelib/rtm_glsl/wrapper/VertexBufferObject.kt
+++ src/main/kotlin/dev/siro256/forgelib/rtm_glsl/wrapper/VertexBufferObject.kt
@@ -5,19 +5,43 @@ import dev.siro256.forgelib.rtm_glsl.enum.GLBufferUsage
 import org.lwjgl.opengl.GL15
 import org.lwjgl.opengl.GL43
 
-abstract class VertexBufferObject(usage: GLBufferUsage, vertices: FloatArray, private val stride: Int) {
+abstract class VertexBufferObject private constructor(private val stride: Int) {
     @Suppress("MemberVisibilityCanBePrivate")
     protected val name = GL15.glGenBuffers()
 
     open fun bind(index: Int) = GL43.glBindVertexBuffer(index, name, 0, stride)
 
-    init {
+    constructor(usage: GLBufferUsage, data: FloatArray, stride: Int): this(stride) {
         if (name == 0) throw RuntimeException("glGenBuffers returns 0")
 
         GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, name)
         GL15.glBufferData(
             GL15.GL_ARRAY_BUFFER,
-            BufferAllocator.createDirectFloatBuffer(vertices.size).put(vertices).apply { rewind() },
+            BufferAllocator.createDirectFloatBuffer(data.size).put(data).apply { rewind() },
+            usage.constant
+        )
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0)
+    }
+
+    constructor(usage: GLBufferUsage, data: IntArray, stride: Int): this(stride) {
+        if (name == 0) throw RuntimeException("glGenBuffers returns 0")
+
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, name)
+        GL15.glBufferData(
+            GL15.GL_ARRAY_BUFFER,
+            BufferAllocator.createDirectIntBuffer(data.size).put(data).apply { rewind() },
+            usage.constant
+        )
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0)
+    }
+
+    constructor(usage: GLBufferUsage, data: ByteArray, stride: Int): this(stride) {
+        if (name == 0) throw RuntimeException("glGenBuffers returns 0")
+
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, name)
+        GL15.glBufferData(
+            GL15.GL_ARRAY_BUFFER,
+            BufferAllocator.createDirectBuffer(data.size).put(data).apply { rewind() },
             usage.constant
         )
         GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0)
