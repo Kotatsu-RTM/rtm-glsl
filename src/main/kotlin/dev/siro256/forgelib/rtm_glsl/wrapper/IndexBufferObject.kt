38459a30e5b8a95e27b952aa162f07fc34a3ff3e
Siro_256 <siro@siro256.dev>
2023-04-23 21:12:52 +0900

    make IndexBufferObject open

--- src/main/kotlin/dev/siro256/forgelib/rtm_glsl/wrapper/IndexBufferObject.kt
+++ src/main/kotlin/dev/siro256/forgelib/rtm_glsl/wrapper/IndexBufferObject.kt
@@ -4,10 +4,11 @@ import dev.siro256.forgelib.rtm_glsl.BufferAllocator
 import dev.siro256.forgelib.rtm_glsl.enum.GLBufferUsage
 import org.lwjgl.opengl.GL15
 
-class IndexBufferObject(usage: GLBufferUsage, indices: IntArray) {
-    private val name = GL15.glGenBuffers()
+open class IndexBufferObject(usage: GLBufferUsage, indices: IntArray) {
+    @Suppress("MemberVisibilityCanBePrivate")
+    protected val name = GL15.glGenBuffers()
 
-    fun bind() = GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, name)
+    open fun bind() = GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, name)
 
     init {
         if (name == 0) throw RuntimeException("glGenBuffers returns 0")
