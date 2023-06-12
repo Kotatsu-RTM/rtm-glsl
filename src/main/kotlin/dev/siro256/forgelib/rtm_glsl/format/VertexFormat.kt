37b1d3b36da86f55dbcefdc6ec1dab09135dfdde
Siro_256 <siro@siro256.dev>
2023-06-12 23:21:09 +0900

    Create new api for VertexFormat

--- src/main/kotlin/dev/siro256/forgelib/rtm_glsl/format/VertexFormat.kt
+++ src/main/kotlin/dev/siro256/forgelib/rtm_glsl/format/VertexFormat.kt
@@ -4,8 +4,12 @@ package dev.siro256.forgelib.rtm_glsl.format
 interface VertexFormat {
     fun toFloatArray(): FloatArray
 
+    fun toList(): List<Float>
+
     data class Vertex(val x: Float, val y: Float, val  z: Float): VertexFormat {
         override fun toFloatArray() = floatArrayOf(x, y, z)
+
+        override fun toList() = listOf(x, y, z)
 
         companion object {
             const val SIZE_ELEMENTS = 3
@@ -15,6 +19,8 @@ interface VertexFormat {
 
     data class VertexUV(val x: Float, val y: Float, val z: Float, val u: Float, val v: Float): VertexFormat {
         override fun toFloatArray() = floatArrayOf(x, y, z, u, v)
+
+        override fun toList() = listOf(x, y, z, u, v)
 
         companion object {
             const val SIZE_ELEMENTS = 5
@@ -28,6 +34,8 @@ interface VertexFormat {
         val u: Float, val v: Float
     ): VertexFormat {
         override fun toFloatArray() = floatArrayOf(vX, vY, vZ, nX, nY, nZ, u, v)
+
+        override fun toList() = listOf(vX, vY, vZ, nX, nY, nZ, u, v)
 
         companion object {
             const val SIZE_ELEMENTS = 8
